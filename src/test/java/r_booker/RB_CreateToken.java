d0bd892c9a8aa07b2f007b22ac9ebb747382659f
Saurabh Kumar Gond <sunnygond619@gmail.com>
2024-08-12 23:56:37 +0530

    restassured file updated

--- src/test/java/r_booker/RB_CreateToken.java
+++ src/test/java/r_booker/RB_CreateToken.java
@@ -1,8 +1,13 @@
 package r_booker;
 
-public class RB_CreateToken {
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.Header;
+import io.restassured.specification.RequestSpecification;
+ class RB_CreateToken {
 	public static void main(String[] args) {
-		
+		baseURI="https://restful-booker.herokuapp.com";
+		given().header(Heade);
 	}
 
 }
